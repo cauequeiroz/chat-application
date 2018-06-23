9f94be9e1b4d24269c8e48f1ecca9767c26797c5
Caue Queiroz <cauenqueiroz@gmail.com>
2018-06-23 20:19:29 -0300

    Create basic client structure

---
+++ src/br/com/cauequeiroz/Client.java
@@ -0,0 +1,36 @@
+package br.com.cauequeiroz;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+	public static void main(String[] args) {
+		
+		System.out.println("Chat Application (Client)\n\n");
+		
+		try {
+		
+			Socket client = new Socket("127.0.0.1", 8080);
+			System.out.println("[Client] Client connected!");
+			
+			PrintStream clientMessages = new PrintStream(client.getOutputStream());
+			
+			Scanner keyboard = new Scanner(System.in);
+			
+			while (keyboard.hasNextLine()) {
+				String message = keyboard.nextLine();
+				clientMessages.println(message);				
+			}
+			
+			keyboard.close();
+			clientMessages.close();
+			client.close();			
+			
+		} catch (IOException e) {
+			System.out.println("[Error] Client error: " + e.getMessage());
+		}
+		
+	}
+}
