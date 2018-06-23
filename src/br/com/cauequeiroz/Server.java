6a1e81d189635bc94a6a5b643de29979cb207c37
Caue Queiroz <cauenqueiroz@gmail.com>
2018-06-23 20:09:21 -0300

    Create basic server structure

---
+++ src/br/com/cauequeiroz/Server.java
@@ -0,0 +1,36 @@
+package br.com.cauequeiroz;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+	public static void main(String[] args) {
+		
+		System.out.println("Chat Application (Server)\n\n");
+		
+		try {
+			
+			ServerSocket server = new ServerSocket(8080);
+			System.out.println("[Server] Server running at port 8080...");
+			
+			Socket client = server.accept();
+			System.out.println("[Server] Client connected!");
+			
+			Scanner clientMessages = new Scanner(client.getInputStream());
+			
+			while (clientMessages.hasNextLine()) {
+				System.out.println(clientMessages.nextLine());
+			}
+			
+			clientMessages.close();
+			client.close();
+			server.close();
+			
+		} catch (IOException e) {
+			System.out.println("[Error] Server error: " + e.getMessage());
+		}
+		
+	}
+}
